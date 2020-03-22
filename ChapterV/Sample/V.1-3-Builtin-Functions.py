7d4d2a1edd1e486b52d410b119cfbdd2fb579089
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-03-22 09:40:25 +0800

    modify

--- ChapterV/Sample/V.1-3-Builtin-Functions.py
+++ ChapterV/Sample/V.1-3-Builtin-Functions.py
@@ -1,4 +1,10 @@
 ls = [1, 2, 5, 0]
+"""
+all(), any()
+hash(), id() 哈希和内存地址
+reversed(), sorted()  逆序和排序
+type() 返回类型
+"""
 print(all(ls))
 print(any(ls))
 print(hash('中国梦'))
@@ -6,3 +12,4 @@ print(id(ls))
 print(list(reversed(ls)))
 print(sorted(ls))
 print(sorted(ls, reverse=True))
+print(type(ls))
